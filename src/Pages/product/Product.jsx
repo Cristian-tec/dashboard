a044a73cad5cbcbca1f392b8b8eb1117d816480b
Cristian-tec <crgs2008@gmail.com>
2023-01-11 10:13:26 -0300

    15

--- src/Pages/product/Product.jsx
+++ src/Pages/product/Product.jsx
@@ -3,103 +3,197 @@ import { Link } from "react-router-dom";
 import "./product.css";
 import Chart from "../../components/chart/Chart";
 import { productData } from "../../dummyData";
+import { useState, useEffect } from "react";
 import { Publish } from "@mui/icons-material";
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 export default function Product(props) {
 
-  const { productId } = useParams(); 
+  const { productId } = useParams();
 
   //console.log(productId);
 
-  const getProductDetail = async () =>{
+  const [data, setData] = useState();
 
-    const response  = await axios.get("https://test-deploy-production-3b4b.up.railway.app/products/2")
+  const getProductDetail = async () => {
+
+    const response = await axios.get(`https://test-deploy-production-3b4b.up.railway.app/products/${productId}`)
     console.log(response.data);
+    setData(response.data);
   }
 
-  getProductDetail();
 
 
-  return (
-    <div className="product">
-      <div className="productTitleContainer">
-        <h1 className="productTitle">Product</h1>
-        <Link to="/newProduct">
-          <button className="productAddButton">Create</button>
-        </Link>
-      </div>
-      <div className="productTop">
-        <div className="productTopLeft">
-          <Chart
-            data={productData}
-            dataKey="Sales"
-            title="Sales Performance"
-            className="productChart"
-          />
+  useEffect(() => {
+
+    getProductDetail();
+
+  }, [])
+
+  if (data) {
+
+    return (
+      <div className="product">
+        <div className="productTitleContainer">
+          <h1 className="productTitle">Product</h1>
+          <Link to="/newProduct">
+            <button className="productAddButton">Create</button>
+          </Link>
         </div>
-        <div className="productTopRight">
-          <div className="productInfoTop">
-            <img
-              src="https://res.cloudinary.com/dqdrtbtuv/image/upload/v1672494806/Dashboard/MME73_AV1_q0zb6i.jpg"
-              alt=""
-              className="productInfoImg"
+        <div className="productTop">
+          <div className="productTopLeft">
+            <Chart
+              data={productData}
+              dataKey="Sales"
+              title="Sales Performance"
+              className="productChart"
             />
-            <span className="productName">Apple Airpods</span>
           </div>
-          <div className="productInfoBottom">
-            <div className="productInfoItem">
-              <span className="prductInfoKey">id:</span>
-              <span className="productInfoValue">123</span>
+          <div className="productTopRight">
+            <div className="productInfoTop">
+              <img
+                src={data[0].image}
+                alt=""
+                className="productInfoImg"
+              />
+              <span className="productName">{data[0].name}</span>
             </div>
-            <div className="productInfoItem">
-              <span className="prductInfoKey">sales:</span>
-              <span className="productInfoValue">$123</span>
-            </div>
-            <div className="productInfoItem">
-              <span className="prductInfoKey">active:</span>
-              <span className="productInfoValue">yes</span>
-            </div>
-            <div className="productInfoItem">
-              <span className="prductInfoKey">in stock:</span>
-              <span className="productInfoValue">No</span>
+            <div className="productInfoBottom">
+              <div className="productInfoItem">
+                <span className="prductInfoKey">id:</span>
+                <span className="productInfoValue">{data[0].id}</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">sales:</span>
+                <span className="productInfoValue">$123</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">active:</span>
+                <span className="productInfoValue">yes</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">in stock:</span>
+                <span className="productInfoValue">No</span>
+              </div>
             </div>
           </div>
         </div>
+        <div className="productBottom">
+          <form className="productForm">
+            <div className="productFormLeft">
+              <label>{data[0].name}</label>
+              <input type="text" placeholder={data[0].name} />
+              <label>In Stock</label>
+              <select name="inStock" id="inStock">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <label>Active</label>
+              <select name="Active" id="Active">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </div>
+            <div className="productFormRight">
+              <div className="productUpload">
+                <img
+                  src={data[0].image}
+                  alt=""
+                  className="productUploadImg"
+                />
+                <label for="file">
+                  <Publish />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <button className="productButton">Update</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="productBottom">
-        <form className="productForm">
-          <div className="productFormLeft">
-            <label>Product Name</label>
-            <input type="text" placeholder="AppleAirpod" />
-            <label>In Stock</label>
-            <select name="inStock" id="inStock">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-            <label>Active</label>
-            <select name="Active" id="Active">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
+    );
+
+  } else {
+    return (
+      <div className="product">
+        <div className="productTitleContainer">
+          <h1 className="productTitle">Product</h1>
+          <Link to="/newProduct">
+            <button className="productAddButton">Create</button>
+          </Link>
+        </div>
+        <div className="productTop">
+          <div className="productTopLeft">
+            <Chart
+              data={productData}
+              dataKey="Sales"
+              title="Sales Performance"
+              className="productChart"
+            />
           </div>
-          <div className="productFormRight">
-            <div className="productUpload">
+          <div className="productTopRight">
+            <div className="productInfoTop">
               <img
                 src="https://res.cloudinary.com/dqdrtbtuv/image/upload/v1672494806/Dashboard/MME73_AV1_q0zb6i.jpg"
                 alt=""
-                className="productUploadImg"
+                className="productInfoImg"
               />
-              <label for="file">
-                <Publish />
-              </label>
-              <input type="file" id="file" style={{ display: "none" }} />
+              <span className="productName">Loading....</span>
             </div>
-            <button className="productButton">Update</button>
+            <div className="productInfoBottom">
+              <div className="productInfoItem">
+                <span className="prductInfoKey">id:</span>
+                <span className="productInfoValue">123</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">salesXXXXXX:</span>
+                <span className="productInfoValue">$123</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">active:</span>
+                <span className="productInfoValue">yes</span>
+              </div>
+              <div className="productInfoItem">
+                <span className="prductInfoKey">in stock:</span>
+                <span className="productInfoValue">No</span>
+              </div>
+            </div>
           </div>
-        </form>
+        </div>
+        <div className="productBottom">
+          <form className="productForm">
+            <div className="productFormLeft">
+              <label>Product Name</label>
+              <input type="text" placeholder="AppleAirpod" />
+              <label>In Stock</label>
+              <select name="inStock" id="inStock">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <label>Active</label>
+              <select name="Active" id="Active">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </div>
+            <div className="productFormRight">
+              <div className="productUpload">
+                <img
+                  src="https://res.cloudinary.com/dqdrtbtuv/image/upload/v1672494806/Dashboard/MME73_AV1_q0zb6i.jpg"
+                  alt=""
+                  className="productUploadImg"
+                />
+                <label for="file">
+                  <Publish />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <button className="productButton">Update</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
